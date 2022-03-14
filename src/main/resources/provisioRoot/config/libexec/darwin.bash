6c6e034ca2a42432b0488e863273fed54b1ab57d
Jason Van Zyl <jason.vanzyl@XD6HRXVCG6.matchtech.net>
2022-03-14 10:09:23 -0400

    Adding support for m1

--- src/main/resources/provisioRoot/config/libexec/darwin.bash
+++ src/main/resources/provisioRoot/config/libexec/darwin.bash
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Checking prerequistes for OSX..."
+# Notes:
+# https://stackoverflow.com/questions/64963370/error-cannot-install-in-homebrew-on-arm-processor-in-intel-default-prefix-usr
+
+ARCH="$(uname -m)"
+echo "Checking prerequistes for OSX on ${ARCH}..."
+if [ "${ARCH}" = "arm64" ]; then
+  echo "Installing Rosetta 2 ..."
+  /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+  brewPrefix="arch -x86_64"
+fi
 
 command -v brew > /dev/null 2>&1
 if [[ $? != 0 ]]; then
   echo "Brew is not installed."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  ${brewPrefix} /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # These scripts themselves require realpath and at the very least jenv
@@ -15,5 +24,5 @@ fi
 # http://www.maizure.org/projects/decoded-gnu-coreutils/
 brew ls --versions coreutils > /dev/null 2>&1
 if [[ $? != 0 ]]; then
-  brew install coreutils
+  ${brewPrefix} brew install coreutils
 fi
