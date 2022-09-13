a2363d842951fdd960fab52f0fd45f6f15d68cb3
Jason van Zyl <jvanzyl@users.noreply.github.com>
2022-09-13 12:36:30 -0400

    Make a symlink for realpath

--- src/main/resources/provisioRoot/config/libexec/darwin.bash
+++ src/main/resources/provisioRoot/config/libexec/darwin.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 PROVISIO_ROOT="${1:-$HOME/.provisio}"
+commonBin="${PROVISIO_ROOT}/bin/common"
+
 echo "Checking prerequistes for OSX ..."
 if [ ! -f ${PROVISIO_ROOT}/bin/common/coreutils ]; then
   echo "GNU coreutils is not installed. Installing GNU coreutils ..."
@@ -16,15 +18,22 @@ if [ ! -f ${PROVISIO_ROOT}/bin/common/coreutils ]; then
   # Rust-based GNU coreutils that provides things like readlink:
   # https://github.com/uutils/coreutils
   # ----------------------------------------------------------------------------
-  version="0.0.12"
+  version="0.0.15"
   base="coreutils-${version}-x86_64-apple-darwin"
   tgz="${base}.tar.gz"
   cachePath="${PROVISIO_ROOT}/bin/cache/coreutils/${version}"
   tgzPath="${cachePath}/${tgz}"
   mkdir -p ${cachePath} > /dev/null 2>&1
   curl -sL -o ${tgzPath} https://github.com/uutils/coreutils/releases/download/${version}/${tgz}
-  commonBin="${PROVISIO_ROOT}/bin/common"
   mkdir -p ${commonBin} > /dev/null 2>&1
   tar -xvf ${tgzPath} --strip-components=1 -C ${commonBin} ${base}/coreutils
   spctl --add "${commonBin}/coreutils"
+fi
+
+# There is only a x86_64 multipath binary for coreutils and multipath capability
+# seems not to work on arm64 machines so we'll create a symlink for realpath
+# just in case for now until an arm64 binary is released.
+if [ ! -L "${commonBin}/realpath" ]; then
+  echo "Creating symlink for realpath ..."
+  ln -s "${commonBin}/coreutils" "${commonBin}/realpath"
 fi
